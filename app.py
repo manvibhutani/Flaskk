1d5b6c770c140bd3f41ffa350a427fb592c08cda
manvibhutani <125116772+manvibhutani@users.noreply.github.com>
2024-09-01 03:24:51 +0530

    hi

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
 
 @app.route('/youtube',methods=["POST"])
 def youtube_sumarizer():
